718f4ace8c9e7e804e372d28f06743d1b4aea9cd
gitSaiev <saievmuhammad@mail.ru>
2022-02-20 14:41:00 +0300

    end commit

---
+++ ng-commands.sh
@@ -0,0 +1,12 @@
+ng new crud_frontend
+cd crud_frontend
+npm i bootstrap
+npm i @fortawesome/fontawesome-free
+
+ng g component header
+ng g component footer
+ng g component user-list
+ng g component user-form
+ng g service service/user-service
+
+ng serve
